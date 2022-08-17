74d6a6ba95a08a6dfce72548d0cca0dca7849ac4
Erzhan <erzhanmuratov99@gmail.com>
2022-08-17 21:24:43 +0600

    parse all available pages and save file

--- parser.py
+++ parser.py
@@ -1,14 +1,24 @@
 import requests
+import csv
+import subprocess, sys
 from bs4 import BeautifulSoup
 
-URL = 'https://auto.ria.com/newauto/marka-mercedes-benz/'
 HEADERS = {'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) '
                         'Chrome/98.0.4758.109 Safari/537.36', 'accept':'*/*'}
 HOST = 'https://auto.ria.com'
+FILE = 'cars.csv'
 
 def get_html(url, params=None):
     r = requests.get(url, headers=HEADERS, params=params)
     return r
+
+def get_pages_count(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    pagination = soup.find_all('span', class_='mhide')
+    if pagination:
+        return int(pagination[-1].get_text())
+    else:
+        return 1
 
 def get_content(html):
     soup = BeautifulSoup(html, 'html.parser')
@@ -31,12 +41,34 @@ def get_content(html):
         })
     return cars
 
+def save_file(items, path):
+    with open(path, 'w', encoding = 'utf-8-sig', newline='') as file:
+        writer = csv.writer(file, delimiter=';')
+        writer.writerow(['Brand', 'Link', 'USD Price', 'UAH Price', 'City'])
+        for item in items:
+            writer.writerow([item['title'], item['link'], item['usd_price'], item['uah_price'], item['city']])
+
+
 def parse():
+    URL = input('Enter a url: ').strip()
     html = get_html(URL)
     if html.status_code == 200:
-        cars = get_content(html.text)
-        return cars
+        cars = []
+        pages_count = get_pages_count(html.text)
+        for page in range(1, pages_count + 1):
+            print(f"Parsing of page {page} out of {pages_count}...")
+            html = get_html(URL, params={'page':page})
+            cars.extend(get_content(html.text))
+        save_file(cars, FILE)
+
+        if len(cars) > 1:
+            print(f"Got {(len(cars))} cars!")
+        else:
+            print(f"Got 1 car!")
+
+        opener = "open" if sys.platform == "darwin" else "xdg-open"
+        subprocess.call([opener, FILE])
     else:
         print("Error")
 
-print(parse())
+parse()
